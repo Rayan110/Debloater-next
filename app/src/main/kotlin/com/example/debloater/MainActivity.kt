f95053938677a1357ceadc70ce682dac91794a12
DEV <devendar87502@gmail.com>
2026-01-03 11:09:25 +0530

    Create MainActivity.kt

---
+++ app/src/main/kotlin/com/example/debloater/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.debloater
+
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: AppAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ShizukuManager.init(this)
+        ShizukuManager.bindService()  // Attempt to bind (will check permission)
+
+        recyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val apps = getInstalledApps()
+        adapter = AppAdapter(apps) { packageName, action ->
+            when (action) {
+                "uninstall" -> ShizukuManager.uninstall(packageName)
+                "disable" -> ShizukuManager.disable(packageName)
+            }
+        }
+        recyclerView.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        ShizukuManager.cleanup()
+        super.onDestroy()
+    }
+
+    private fun getInstalledApps(): List<PackageInfo> {
+        val pm = packageManager
+        return pm.getInstalledPackages(PackageManager.MATCH_ALL).sortedBy { pm.getApplicationLabel(it.applicationInfo).toString() }
+    }
+}
