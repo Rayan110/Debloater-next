68d5be257ac4faaf61657b877fd81d602e141a61
DEV <devendar87502@gmail.com>
2026-01-03 11:06:46 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
+// libs.versions.toml is not used here for simplicity; versions are hardcoded.
